d65c6d734973991d5f981bbd573ccdc007a141ad
omolo-j <omoloj135@gmail.com>
2024-05-30 21:11:56 +0300

    file-io-lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = Path(str(file_name) + '.txt')
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = Path(str(file_name) + '.txt')
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    if not str(file_name).endswith('.txt'):
+        file_name = Path(str(file_name) + '.txt')
+    with open(file_name, 'r') as file:
+        content = file.read()
+    return content
